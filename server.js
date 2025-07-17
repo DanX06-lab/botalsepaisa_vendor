9993d5461950136aa0889694850e71e0629e919e
Souvik <souvikbiswas2045@gmail.com>
2025-07-17 17:26:53 +0530

    frontend connected

--- server.js
+++ server.js
@@ -15,7 +15,8 @@ app.use(express.json());
 app.use(cors({
   origin: [
     'http://localhost:3000',
-    'http://127.0.0.1:3000'
+    'http://127.0.0.1:3000',
+    process.env.FRONTEND_URL
   ],
   credentials: true
 }));
